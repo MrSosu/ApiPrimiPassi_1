757b5dec3d912982156ceb7106e72efb71dd1dd8
MrSosu <49311976+MrSosu@users.noreply.github.com>
2022-12-13 17:44:54 +0100

    Update NameController.java

--- src/main/java/it/Develhope/ApiPrimiPassi/Controller/NameController.java
+++ src/main/java/it/Develhope/ApiPrimiPassi/Controller/NameController.java
@@ -3,18 +3,16 @@ package it.Develhope.ApiPrimiPassi.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping(value = "/name")
 public class NameController {
 
-    @GetMapping(value = "/getName/{name}")
-    public String getName(@PathVariable String name){
-        return name;
+    @GetMapping
+    public String getName(){
+        return "Claudio";
     }
 
-    @PostMapping(value = "/reverseName/{name}")
-    public StringBuilder reverseName(@PathVariable String name){
-        StringBuilder reverseName = new StringBuilder(name);
-        reverseName.reverse();
-        return reverseName;
+    @PostMapping
+    public String reverseName(){
+        return "oidualC;
     }
 }
